6b58da8dfa0fe898960391c6ce169e88c9b932fd
Romaric Pascal <hello@romaricpascal.is>
2020-04-22 19:10:14 +0100

    feat: implement chain of responsibility proxy

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,3 +1,53 @@
 const test = require('ava');
+const { chainOfResponsibility, compute, collect } = require('..');
 
-test('ava is setup', (t) => t.pass());
+test('it return existing properties', (t) => {
+  const proxy = chainOfResponsibility({ property: 'value' });
+  t.is(proxy.property, 'value');
+});
+test('it implements a chain of responsibility', (t) => {
+  const proxy = chainOfResponsibility([{}, { property: 'value' }]);
+  t.is(proxy.property, 'value');
+});
+test('it iterates first to last', (t) => {
+  const proxy = chainOfResponsibility([
+    { property: 'value' },
+    { property: 'otherValue' },
+  ]);
+  t.is(proxy.property, 'value');
+});
+test('it allows to declare computed properties', (t) => {
+  const proxy = chainOfResponsibility([
+    {},
+    { property: compute(() => 'value') },
+  ]);
+  t.is(proxy.property, 'value');
+});
+test('it allows using the proxy for computing values', (t) => {
+  const proxy = chainOfResponsibility([
+    { a: 1 },
+    {
+      b: 3,
+      c: compute((data) => data.a + data.b),
+    },
+  ]);
+  t.is(proxy.c, 4);
+});
+test('it provides a way to grab the next value in the chain to computed properties', (t) => {
+  const proxy = chainOfResponsibility([
+    {
+      property: compute((data, next) => data.base + next().value),
+    },
+    {
+      base: 'hello',
+    },
+    {
+      property: ' world',
+    },
+  ]);
+  t.is(proxy.property, 'hello world');
+});
+test('it provides a way to grab all the values of a given property', (t) => {
+  const proxy = chainOfResponsibility([{ a: collect }, { a: 1 }, { a: 2 }]);
+  t.deepEqual(proxy.a, [1, 2]);
+});
--- src/index.js
+++ src/index.js
@@ -1,2 +1,77 @@
-// Code goes here
-console.log('Hello, world');
+const SYMBOL_COMPUTED = Symbol('computed');
+const SYMBOL_COLLECT = Symbol('collect');
+
+exports.chainOfResponsibility = function chainOfResponsibility(sources) {
+  const proxy = new Proxy(sources, {
+    get: withComputed(withCollect(get)),
+  });
+  return proxy;
+
+  function withComputed(getter) {
+    return function (sources, property) {
+      const { value, next } = getter(sources, property);
+      if (value && value[SYMBOL_COMPUTED]) {
+        return value(proxy, next);
+      }
+      return value;
+    };
+  }
+};
+
+function withCollect(getter) {
+  return function (sources, property) {
+    const result = getter(sources, property);
+    if (result.value === SYMBOL_COLLECT) {
+      return collect(sources, property, result.index + 1);
+    }
+    return result;
+  };
+}
+
+exports.collect = SYMBOL_COLLECT;
+
+exports.compute = function compute(computation) {
+  const fn = function (...args) {
+    return computation(...args);
+  };
+
+  fn[SYMBOL_COMPUTED] = true;
+
+  return fn;
+};
+
+function get(sources, property) {
+  if (Array.isArray(sources)) {
+    return first(sources, property);
+  }
+  return { value: sources[property], next() {}, index: sources.length };
+}
+
+function first(sources, property, startIndex = 0) {
+  if (startIndex < sources.length) {
+    for (var index = startIndex; index < sources.length; index++) {
+      if (property in sources[index]) {
+        return {
+          value: sources[index][property],
+          next: function () {
+            return first(sources, property, (startIndex = index + 1));
+          },
+          index: index,
+        };
+      }
+    }
+  }
+  return { value: undefined, next() {}, index: sources.length };
+}
+
+function collect(sources, property, fromIndex = 0) {
+  const result = [];
+  if (fromIndex < sources.length) {
+    for (var index = fromIndex; index < sources.length; index++) {
+      if (property in sources[index]) {
+        result.push(sources[index][property]);
+      }
+    }
+  }
+  return { value: result, next: () => {}, index: sources.length };
+}
